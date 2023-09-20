68809027b21fe6a795d9a35a77812e685994b666
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-21 00:27:34 +0400

    header fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DesignSystemModule } from './design-system/design-system.module'
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/design-system/button/button.component.ts
+++ src/app/design-system/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit{
   @Input() public buttonText = "";
   @Input() public buttonBg = "";
+  @Input() public buttonTextSize = "";
   constructor(){}
   ngOnInit(): void {
     
