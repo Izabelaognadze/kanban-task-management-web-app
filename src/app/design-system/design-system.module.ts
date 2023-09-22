41e79f21e39716517f7334a1fae400d0c9333bc8
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-22 23:47:55 +0400

    sidebar creating

--- src/app/design-system/design-system.module.ts
+++ src/app/design-system/design-system.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { SwitchComponent } from './switch/switch.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    SwitchComponent
+    SwitchComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
